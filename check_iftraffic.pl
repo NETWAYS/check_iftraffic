552832bb1c9788c1070b50826e9a9530211d14e7
Michael Friedrich <michael.friedrich@netways.de>
2019-06-03 10:36:19 +0200

    Quality: Run perltidy

--- check_iftraffic.pl
+++ check_iftraffic.pl
@@ -76,11 +76,10 @@ my $response;
 # Path to  tmp files
 my $TRAFFIC_FILE = "/tmp/traffic";
 
-my %STATUS_CODE =
-  ( 'UNKNOWN' => '-1', 'OK' => '0', 'WARNING' => '1', 'CRITICAL' => '2' );
+my %STATUS_CODE = ('UNKNOWN' => '-1', 'OK' => '0', 'WARNING' => '1', 'CRITICAL' => '2');
 
 #default values;
-my ( $in_bytes, $out_bytes ) = 0;
+my ($in_bytes, $out_bytes) = 0;
 my $warn_usage = 85;
 my $crit_usage = 98;
 my $COMMUNITY  = "public";
@@ -89,73 +88,70 @@ my $max_value;
 my $max_bytes;
 
 my $status = GetOptions(
-	"h|help"        => \$opt_h,
-	"C|community=s" => \$COMMUNITY,
-	"V|version=s"	=> \$snmp_version,
-	"w|warning=s"   => \$warn_usage,
-	"c|critical=s"  => \$crit_usage,
-	"b|bandwidth=i" => \$iface_speed,
-	"p|port=i"      => \$port,
-	"u|units=s"     => \$units,
-	"i|interface=s" => \$iface_descr,
-	"H|hostname=s"  => \$host_address,
-	"M|max=i" 	=> \$max_value
+    "h|help"        => \$opt_h,
+    "C|community=s" => \$COMMUNITY,
+    "V|version=s"   => \$snmp_version,
+    "w|warning=s"   => \$warn_usage,
+    "c|critical=s"  => \$crit_usage,
+    "b|bandwidth=i" => \$iface_speed,
+    "p|port=i"      => \$port,
+    "u|units=s"     => \$units,
+    "i|interface=s" => \$iface_descr,
+    "H|hostname=s"  => \$host_address,
+    "M|max=i"       => \$max_value
 );
 
-if ( $status == 0 ) {
-	print_help();
-	exit $STATUS_CODE{'OK'};
+if ($status == 0) {
+    print_help();
+    exit $STATUS_CODE{'OK'};
 }
 
-if ( ( !$host_address ) or ( !$iface_descr ) or ( !$iface_speed ) ) {
-	print_usage();
+if ((!$host_address) or (!$iface_descr) or (!$iface_speed)) {
+    print_usage();
 }
 
-$iface_speed = bits2bytes( $iface_speed, $units ) / 1024;
-if ( !$max_value ) {
+$iface_speed = bits2bytes($iface_speed, $units) / 1024;
+if (!$max_value) {
 
-	#if no -M Parameter was set, set it to 32Bit Overflow
-	$max_bytes = 419304;    # the value is (2^32/1024)
-}
-else {
-	$max_bytes = unit2bytes( $max_value, $units );
-}
-
-if ( $snmp_version =~ /[12]/ ) {
-	( $session, $error ) = Net::SNMP->session(
-		-hostname  => $host_address,
-		-community => $COMMUNITY,
-		-port      => $port,
-		-version   => $snmp_version
-	);
-
-	if ( !defined($session) ) {
-		print("UNKNOWN: $error");
-		exit $STATUS_CODE{'UNKNOWN'};
-	}
-}
-elsif ( $snmp_version =~ /3/ ) {
-	my $state = 'UNKNOWN';
-	print("$state: No support for SNMP v3 yet\n");
-	exit $STATUS_CODE{$state};
-}
-else {
-	my $state = 'UNKNOWN';
-	print("$state: No support for SNMP v$snmp_version yet\n");
-	exit $STATUS_CODE{$state};
+    #if no -M Parameter was set, set it to 32Bit Overflow
+    $max_bytes = 419304;    # the value is (2^32/1024)
+} else {
+    $max_bytes = unit2bytes($max_value, $units);
 }
 
-$iface_number = fetch_ifdescr( $session, $iface_descr );
+if ($snmp_version =~ /[12]/) {
+    ($session, $error) = Net::SNMP->session(
+        -hostname  => $host_address,
+        -community => $COMMUNITY,
+        -port      => $port,
+        -version   => $snmp_version
+    );
 
-push( @snmpoids, $snmpIfInOctets . "." . $iface_number );
-push( @snmpoids, $snmpIfOutOctets . "." . $iface_number );
+    if (!defined($session)) {
+        print("UNKNOWN: $error");
+        exit $STATUS_CODE{'UNKNOWN'};
+    }
+} elsif ($snmp_version =~ /3/) {
+    my $state = 'UNKNOWN';
+    print("$state: No support for SNMP v3 yet\n");
+    exit $STATUS_CODE{$state};
+} else {
+    my $state = 'UNKNOWN';
+    print("$state: No support for SNMP v$snmp_version yet\n");
+    exit $STATUS_CODE{$state};
+}
 
-if ( !defined( $response = $session->get_request(@snmpoids) ) ) {
-	my $answer = $session->error;
-	$session->close;
+$iface_number = fetch_ifdescr($session, $iface_descr);
 
-	print("WARNING: SNMP error: $answer\n");
-	exit $STATUS_CODE{'WARNING'};
+push(@snmpoids, $snmpIfInOctets . "." . $iface_number);
+push(@snmpoids, $snmpIfOutOctets . "." . $iface_number);
+
+if (!defined($response = $session->get_request(@snmpoids))) {
+    my $answer = $session->error;
+    $session->close;
+
+    print("WARNING: SNMP error: $answer\n");
+    exit $STATUS_CODE{'WARNING'};
 }
 
 $in_bytes  = $response->{ $snmpIfInOctets . "." . $iface_number } / 1024;
@@ -168,164 +164,154 @@ my $last_check_time = time - 1;
 my $last_in_bytes   = $in_bytes;
 my $last_out_bytes  = $out_bytes;
 
-if (
-	open( FILE,
-		"<" . $TRAFFIC_FILE . "_if" . $iface_number . "_" . $host_address
-	)
-  )
-{
-	while ( $row = <FILE> ) {
-		chomp($row);
-		( $last_check_time, $last_in_bytes, $last_out_bytes ) =
-		  split( ":", $row );
-	}
-	close(FILE);
+if (open(FILE, "<" . $TRAFFIC_FILE . "_if" . $iface_number . "_" . $host_address)) {
+    while ($row = <FILE>) {
+        chomp($row);
+        ($last_check_time, $last_in_bytes, $last_out_bytes)
+            = split(":", $row);
+    }
+    close(FILE);
 }
 
 my $update_time = time;
 
-open( FILE, ">" . $TRAFFIC_FILE . "_if" . $iface_number . "_" . $host_address )
-  or die "Can't open $TRAFFIC_FILE for writing: $!";
-printf FILE ( "%s:%.0ld:%.0ld\n", $update_time, $in_bytes, $out_bytes );
+open(FILE, ">" . $TRAFFIC_FILE . "_if" . $iface_number . "_" . $host_address)
+    or die "Can't open $TRAFFIC_FILE for writing: $!";
+printf FILE ("%s:%.0ld:%.0ld\n", $update_time, $in_bytes, $out_bytes);
 close(FILE);
 
 my $db_file;
 
 #Check for and correct counter overflow (if possible).
 #See function counter_overflow.
-$in_bytes  = counter_overflow( $in_bytes,  $last_in_bytes,  $max_bytes );
-$out_bytes = counter_overflow( $out_bytes, $last_out_bytes, $max_bytes );
+$in_bytes  = counter_overflow($in_bytes,  $last_in_bytes,  $max_bytes);
+$out_bytes = counter_overflow($out_bytes, $last_out_bytes, $max_bytes);
 
-my $in_traffic = sprintf( "%.2lf",
-	( $in_bytes - $last_in_bytes ) / ( time - $last_check_time ) );
-my $out_traffic = sprintf( "%.2lf",
-	( $out_bytes - $last_out_bytes ) / ( time - $last_check_time ) );
+my $in_traffic  = sprintf("%.2lf", ($in_bytes - $last_in_bytes) /   (time - $last_check_time));
+my $out_traffic = sprintf("%.2lf", ($out_bytes - $last_out_bytes) / (time - $last_check_time));
 
-my $in_traffic_absolut  = sprintf( "%.0d", $last_in_bytes );
-my $out_traffic_absolut = sprintf( "%.0d", $last_out_bytes );
+my $in_traffic_absolut  = sprintf("%.0d", $last_in_bytes);
+my $out_traffic_absolut = sprintf("%.0d", $last_out_bytes);
 
-my $in_usage  = sprintf( "%.1f", ( 1.0 * $in_traffic * 100 ) / $iface_speed );
-my $out_usage = sprintf( "%.1f", ( 1.0 * $out_traffic * 100 ) / $iface_speed );
+my $in_usage  = sprintf("%.1f", (1.0 * $in_traffic * 100) / $iface_speed);
+my $out_usage = sprintf("%.1f", (1.0 * $out_traffic * 100) / $iface_speed);
 
 my $in_prefix  = "k";
 my $out_prefix = "k";
 
-if ( $in_traffic > 1024 ) {
-	$in_traffic = sprintf( "%.2f", $in_traffic / 1024 );
-	$in_prefix = "M";
+if ($in_traffic > 1024) {
+    $in_traffic = sprintf("%.2f", $in_traffic / 1024);
+    $in_prefix = "M";
 }
 
-if ( $out_traffic > 1024 ) {
-	$out_traffic = sprintf( "%.2f", $out_traffic / 1024 );
-	$out_prefix = "M";
+if ($out_traffic > 1024) {
+    $out_traffic = sprintf("%.2f", $out_traffic / 1024);
+    $out_prefix = "M";
 }
 
-$in_bytes  = sprintf( "%.2f", $in_bytes / 1024 );
-$out_bytes = sprintf( "%.2f", $out_bytes / 1024 );
+$in_bytes  = sprintf("%.2f", $in_bytes / 1024);
+$out_bytes = sprintf("%.2f", $out_bytes / 1024);
 
 my $exit_status = "OK";
 
 my $output = "Total RX Bytes: $in_bytes MB, Total TX Bytes: $out_bytes MB<br>";
-$output .=
-    "Average Traffic: $in_traffic "
-  . $in_prefix . "B/s ("
-  . $in_usage
-  . "%) in, $out_traffic "
-  . $out_prefix . "B/s ("
-  . $out_usage
-  . "%) out";
+$output
+    .= "Average Traffic: $in_traffic "
+    . $in_prefix . "B/s ("
+    . $in_usage
+    . "%) in, $out_traffic "
+    . $out_prefix . "B/s ("
+    . $out_usage
+    . "%) out";
 
-if ( ( $in_usage > $crit_usage ) or ( $out_usage > $crit_usage ) ) {
-	$exit_status = "CRITICAL";
+if (($in_usage > $crit_usage) or ($out_usage > $crit_usage)) {
+    $exit_status = "CRITICAL";
 }
 
-if ((($in_usage > $warn_usage) or ($out_usage > $warn_usage ))
-  && $exit_status eq "OK" ) {
-	$exit_status = "WARNING";
+if ((($in_usage > $warn_usage) or ($out_usage > $warn_usage))
+    && $exit_status eq "OK")
+{
+    $exit_status = "WARNING";
 }
 
 $output .= "<br>$exit_status bandwidth utilization.\n"
-  if ( $exit_status ne "OK" );
+    if ($exit_status ne "OK");
 
-$output .=
-"|inUsage=${in_usage}%;${warn_usage};${crit_usage} outUsage=${out_usage}%;${warn_usage};${crit_usage} "
-  . "inAbsolut=${in_traffic_absolut}c outAbsolut=${out_traffic_absolut}c";
+$output .= "|inUsage=${in_usage}%;${warn_usage};${crit_usage} outUsage=${out_usage}%;${warn_usage};${crit_usage} "
+    . "inAbsolut=${in_traffic_absolut}c outAbsolut=${out_traffic_absolut}c";
 
 print $output;
-exit( $STATUS_CODE{$exit_status} );
+exit($STATUS_CODE{$exit_status});
 
 sub fetch_ifdescr {
-	my $state;
-	my $response;
+    my $state;
+    my $response;
 
-	my $snmpkey;
-	my $answer;
-	my $key;
+    my $snmpkey;
+    my $answer;
+    my $key;
 
-	my ( $session, $ifdescr ) = @_;
+    my ($session, $ifdescr) = @_;
 
-	if ( !defined( $response = $session->get_table($snmpIfDescr) ) ) {
-		$answer = $session->error;
-		$session->close;
-		$state = 'CRITICAL';
-		$session->close;
-		exit $STATUS_CODE{$state};
-	}
+    if (!defined($response = $session->get_table($snmpIfDescr))) {
+        $answer = $session->error;
+        $session->close;
+        $state = 'CRITICAL';
+        $session->close;
+        exit $STATUS_CODE{$state};
+    }
 
-	foreach $key ( keys %{$response} ) {
-		if ( $response->{$key} =~ /^$ifdescr$/ ) {
-			$key =~ /.*\.(\d+)$/;
-			$snmpkey = $1;
+    foreach $key (keys %{$response}) {
+        if ($response->{$key} =~ /^$ifdescr$/) {
+            $key =~ /.*\.(\d+)$/;
+            $snmpkey = $1;
 
-			# print "$ifdescr = $key / $snmpkey \n";  #debug
-		}
-	}
-	unless ( defined $snmpkey ) {
-		$session->close;
-		$state = 'CRITICAL';
-		printf "$state: Could not match $ifdescr \n";
-		exit $STATUS_CODE{$state};
-	}
-	return $snmpkey;
+            # print "$ifdescr = $key / $snmpkey \n";  #debug
+        }
+    }
+    unless (defined $snmpkey) {
+        $session->close;
+        $state = 'CRITICAL';
+        printf "$state: Could not match $ifdescr \n";
+        exit $STATUS_CODE{$state};
+    }
+    return $snmpkey;
 }
 
 #Converts an input value to value in bits
 sub bits2bytes {
-	return unit2bytes(@_) / 8;
+    return unit2bytes(@_) / 8;
 }
 
 #Converts an input value to value in bytes
 sub unit2bytes {
-	my ( $value, $unit ) = @_;
+    my ($value, $unit) = @_;
 
-	if ( $unit eq "g" ) {
-		return $value * 1024 * 1024 * 1024;
-	}
-	elsif ( $unit eq "m" ) {
-		return $value * 1024 * 1024;
-	}
-	elsif ( $unit eq "k" ) {
-		return $value * 1024;
-	}
-	else {
-		print "You have to supply a supported unit\n";
-		exit $STATUS_CODE{'UNKNOWN'};
-	}
+    if ($unit eq "g") {
+        return $value * 1024 * 1024 * 1024;
+    } elsif ($unit eq "m") {
+        return $value * 1024 * 1024;
+    } elsif ($unit eq "k") {
+        return $value * 1024;
+    } else {
+        print "You have to supply a supported unit\n";
+        exit $STATUS_CODE{'UNKNOWN'};
+    }
 }
 
 #This function detects if an overflow occurs. If so, it returns
 #a computed value for $bytes.
 #If there is no counter overflow it simply returns the origin value of $bytes.
 sub counter_overflow {
-	my ( $bytes, $last_bytes, $max_bytes ) = @_;
+    my ($bytes, $last_bytes, $max_bytes) = @_;
 
-	$bytes += $max_bytes if ( $bytes < $last_bytes );
-	$bytes = 0 if ( $bytes < $last_bytes );
-	return $bytes;
+    $bytes += $max_bytes if ($bytes < $last_bytes);
+    $bytes = 0 if ($bytes < $last_bytes);
+    return $bytes;
 }
 
 sub print_usage {
-	print <<EOU;
+    print <<EOU;
     Usage: check_iftraffic.pl -H host -C community -V snmp_version -i if_descr -b if_max_speed -u unit [ -w warn ] [ -c crit ] [ -M max_counter_value ]
 
     Options:
@@ -354,5 +340,5 @@ sub print_usage {
          check_iftraffic.pl -H localhost -C public -i en0 -b 100 -u m
 EOU
 
-	exit( $STATUS_CODE{"UNKNOWN"} );
+    exit($STATUS_CODE{"UNKNOWN"});
 }
